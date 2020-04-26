5f22b194d57cb1cdd487b46b1f6a494f430a80ac
David Caulfield <djcfield@hotmail.com>
2020-04-26 22:16:58 +0100

    Update ex1_22.scm

--- ex1_22.scm
+++ ex1_22.scm
@@ -1,4 +1,3 @@
-
 ; Given a range of numbers, check if there exist 3 consecutive odd primes in that range.
 
 ; Taken from 1.2.6
@@ -21,41 +20,44 @@
     (= num (smallest-divisor num))
 )
 
-
-
-(define (search start finish)
-
-    ; Given a number n
-    ; If it is prime, check the next 2 numbers
-    ; If they are prime, return them
-    ; If not prime, return 0
-    (cond
-        ; If even, skip
-        ((= 0 (remainder start 2)) (search (+ start 1) finish))
-
-        ; If range is finished, return 0
-        ((= start finish) 0)
-
-        ; If number is prime, check the next 2 odd numbers
-        ((prime? start) 
-            (consec? start)
-        )
-        (else (search (+ start 1) finish))
-    )
-)
-
-; Check if n+2 and n+4 are prime
-(define (consec? n)
+(define (return-prime n)
     (
-    if (prime? (+ n 2))
-        (prime? (+ n 4))
-        (#f)
+    if (prime? n)
+        n
+        0
     )
 )
 
+; This was my first attempt.
+; Something I have forgotten since studying maths in university - ALWAYS READ THE QUESTION!!!
+; The question has asked to find 3 consecutive primes in a given range - it does not say to ONLY find 3 consecutive primes in a given range.
+(define (search-for-primes start finish)
+    (define (search-for-primes-count start finish count)
+        ; Given a number n
+        ; If it is prime, check the next 2 numbers
+        ; If they are prime, return them
+        ; If not prime, return 0
+        (cond
+            ; If range is finished, return 0
+            ((= start finish) 0)
 
+            ; If count=2, return prime number
+            ((= count 2) start)
 
+            ; If number is prime, check the next odd number
+            ((prime? start)
+                start
+            )
 
+            (else (search-for-primes-count (+ start 2) finish 0))
+        )
+    )
 
+    (
+    if (even? start)
+        (search-for-primes-count start finish 0)
+        (search-for-primes-count (+ start 1) finish 0)
+    )
+)
 
-
+; TODO: Go through range of primes and print out each prime number and time according to given code.
